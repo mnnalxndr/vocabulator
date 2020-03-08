6f69c01a64c0e9b64c731838a3d882620f208606
Alex Mann <Alex.Mann@theaccessgroup.com>
2020-03-08 14:27:55 +0000

    Basic api request now working!

--- src/components/search/Definitions.jsx
+++ src/components/search/Definitions.jsx
@@ -3,24 +3,14 @@ import React, { useEffect, useState } from 'react';
 
 
 const Definitions = (props) => {
-  const { searchTerm } = props;
-  const definitionList = [
-    {definition: 'Def 1', example: "Ex 1"},
-    {definition: 'Def 2', example: "Ex 2"},
-    {definition: 'Def 3', example: "Ex 3"}
-  ];
-  // useEffect(() => {
-  //   fetch(`https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${searchTerm}?fields=definitions,examples&strictMatch=true`)
-  //   .then(response => response.json())
-  //   .then()
-  // }, [searchTerm]);
+  const { searchTerm, definitionList } = props;
   
   return (
     <div>
+      {searchTerm}
       {definitionList ?
       definitionList.map(entry => (
         <div className="definition">
-          <div>{searchTerm}</div>
           <div>{entry.definition}</div>
           <div>{entry.example}</div>
         </div>
--- src/components/search/WordSearch.jsx
+++ src/components/search/WordSearch.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Definitions from './Definitions';
 
 const WordSearch = () => {
   const [searchInput, setSearchInput] = useState('');
-  const [searchTerm, setSearchTerm] = useState(searchInput);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [definitions, setDefinitions] = useState();
+
+  
+  useEffect(() => {
+    if (searchTerm.length > 0) {
+      fetch(`https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${searchTerm}?fields=definitions,examples&strictMatch=true`, {
+        headers: {
+          "Accept": "application/json",
+          "app_id": "95321022",
+          "app_key": "67f37c1d36e23e98452025364b6e0af9"
+        }
+      })
+      .then(response => response.json())
+      .then(results => {
+        debugger;
+        setDefinitions(results.results[0].lexicalEntries.map(entry => {
+          const def = entry.entries[0].senses[0]; 
+          return {
+            id: def.id,
+            definition: def.definitions[0],
+            example: def.examples[0].text
+          }
+        }))
+      })
+    }
+  }, [searchTerm]);
 
   return (
     <div>
@@ -15,7 +41,7 @@ const WordSearch = () => {
       <br />
       <button type="button" onClick={() => setSearchTerm(searchInput)}>Define</button>
       <br/>
-      <Definitions searchTerm={searchTerm} />
+      <Definitions searchTerm={searchTerm} definitionList={definitions} />
     </div>
   );
 };
